aaffc48b124b8677f59c9e62349e698ca440fb6b
tiptop <105000222+tiptoppp@users.noreply.github.com>
2022-06-24 09:36:33 +0200

    Add files via upload

---
+++ MemoryNoAdmin/Methods/Read.cs
@@ -0,0 +1,363 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using static MemoryNoAdmin.Imps;
+
+namespace MemoryNoAdmin
+{
+    public partial class Mem
+    {
+        public string CutString(string str)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (char c in str)
+            {
+                if (c >= ' ' && c <= '~')
+                    sb.Append(c);
+                else
+                    break;
+            }
+            return sb.ToString();
+        }
+
+        public byte[] ReadBytes(string code, long length, string file = "")
+        {
+            byte[] memory = new byte[length];
+            UIntPtr theCode = GetCode(code, file);
+            if (theCode == null || theCode == UIntPtr.Zero || theCode.ToUInt64() < 0x10000)
+                return null;
+
+            if (!ReadProcessMemory(mProc.Handle, theCode, memory, (UIntPtr)length, IntPtr.Zero))
+                return null;
+
+            return memory;
+        }
+
+        public float ReadFloat(string code, string file = "", bool round = true)
+        {
+            byte[] memory = new byte[4];
+
+            UIntPtr theCode = GetCode(code, file);
+            if (theCode == null || theCode == UIntPtr.Zero || theCode.ToUInt64() < 0x10000)
+                return 0;
+
+            try
+            {
+                if (ReadProcessMemory(mProc.Handle, theCode, memory, (UIntPtr)4, IntPtr.Zero))
+                {
+                    float address = BitConverter.ToSingle(memory, 0);
+                    float returnValue = (float)address;
+                    if (round)
+                        returnValue = (float)Math.Round(address, 2);
+                    return returnValue;
+                }
+                else
+                    return 0;
+            }
+            catch
+            {
+                return 0;
+            }
+        }
+
+        public string ReadString(string code, string file = "", int length = 32, bool zeroTerminated = true, System.Text.Encoding stringEncoding = null)
+        {
+            if (stringEncoding == null)
+                stringEncoding = System.Text.Encoding.UTF8;
+
+            byte[] memoryNormal = new byte[length];
+            UIntPtr theCode = GetCode(code, file);
+            if (theCode == null || theCode == UIntPtr.Zero || theCode.ToUInt64() < 0x10000)
+                return "";
+
+            if (ReadProcessMemory(mProc.Handle, theCode, memoryNormal, (UIntPtr)length, IntPtr.Zero))
+                return (zeroTerminated) ? stringEncoding.GetString(memoryNormal).Split('\0')[0] : stringEncoding.GetString(memoryNormal);
+            else
+                return "";
+        }
+
+        public double ReadDouble(string code, string file = "", bool round = true)
+        {
+            byte[] memory = new byte[8];
+
+            UIntPtr theCode = GetCode(code, file);
+            if (theCode == null || theCode == UIntPtr.Zero || theCode.ToUInt64() < 0x10000)
+                return 0;
+
+            try
+            {
+                if (ReadProcessMemory(mProc.Handle, theCode, memory, (UIntPtr)8, IntPtr.Zero))
+                {
+                    double address = BitConverter.ToDouble(memory, 0);
+                    double returnValue = (double)address;
+                    if (round)
+                        returnValue = (double)Math.Round(address, 2);
+                    return returnValue;
+                }
+                else
+                    return 0;
+            }
+            catch
+            {
+                return 0;
+            }
+        }
+
+        public int ReadUIntPtr(UIntPtr code)
+        {
+            byte[] memory = new byte[4];
+            if (ReadProcessMemory(mProc.Handle, code, memory, (UIntPtr)4, IntPtr.Zero))
+                return BitConverter.ToInt32(memory, 0);
+            else
+                return 0;
+        }
+
+        public int ReadInt(string code, string file = "")
+        {
+            byte[] memory = new byte[4];
+            UIntPtr theCode = GetCode(code, file);
+            if (theCode == null || theCode == UIntPtr.Zero || theCode.ToUInt64() < 0x10000)
+                return 0;
+
+            if (ReadProcessMemory(mProc.Handle, theCode, memory, (UIntPtr)4, IntPtr.Zero))
+                return BitConverter.ToInt32(memory, 0);
+            else
+                return 0;
+        }
+
+        public long ReadLong(string code, string file = "")
+        {
+            byte[] memory = new byte[16];
+            UIntPtr theCode = GetCode(code, file);
+
+            if (theCode == null || theCode == UIntPtr.Zero || theCode.ToUInt64() < 0x10000)
+                return 0;
+
+            if (ReadProcessMemory(mProc.Handle, theCode, memory, (UIntPtr)8, IntPtr.Zero))
+                return BitConverter.ToInt64(memory, 0);
+            else
+                return 0;
+        }
+
+        public UInt32 ReadUInt(string code, string file = "")
+        {
+            byte[] memory = new byte[4];
+            UIntPtr theCode = GetCode(code, file);
+            if (theCode == null || theCode == UIntPtr.Zero || theCode.ToUInt64() < 0x10000)
+                return 0;
+
+            if (ReadProcessMemory(mProc.Handle, theCode, memory, (UIntPtr)4, IntPtr.Zero))
+                return BitConverter.ToUInt32(memory, 0);
+            else
+                return 0;
+        }
+
+        public int Read2ByteMove(string code, int moveQty, string file = "")
+        {
+            byte[] memory = new byte[4];
+            UIntPtr theCode = GetCode(code, file);
+            if (theCode == null || theCode == UIntPtr.Zero || theCode.ToUInt64() < 0x10000)
+                return 0;
+
+            UIntPtr newCode = UIntPtr.Add(theCode, moveQty);
+
+            if (ReadProcessMemory(mProc.Handle, newCode, memory, (UIntPtr)2, IntPtr.Zero))
+                return BitConverter.ToInt32(memory, 0);
+            else
+                return 0;
+        }
+
+        public int ReadIntMove(string code, int moveQty, string file = "")
+        {
+            byte[] memory = new byte[4];
+            UIntPtr theCode = GetCode(code, file);
+            if (theCode == null || theCode == UIntPtr.Zero || theCode.ToUInt64() < 0x10000)
+                return 0;
+
+            UIntPtr newCode = UIntPtr.Add(theCode, moveQty);
+
+            if (ReadProcessMemory(mProc.Handle, newCode, memory, (UIntPtr)4, IntPtr.Zero))
+                return BitConverter.ToInt32(memory, 0);
+            else
+                return 0;
+        }
+
+        public ulong ReadUIntMove(string code, int moveQty, string file = "")
+        {
+            byte[] memory = new byte[8];
+            UIntPtr theCode = GetCode(code, file, 8);
+            if (theCode == null || theCode == UIntPtr.Zero || theCode.ToUInt64() < 0x10000)
+                return 0;
+
+            UIntPtr newCode = UIntPtr.Add(theCode, moveQty);
+
+            if (ReadProcessMemory(mProc.Handle, newCode, memory, (UIntPtr)8, IntPtr.Zero))
+                return BitConverter.ToUInt64(memory, 0);
+            else
+                return 0;
+        }
+
+        public int Read2Byte(string code, string file = "")
+        {
+            byte[] memoryTiny = new byte[4];
+
+            UIntPtr theCode = GetCode(code, file);
+            if (theCode == null || theCode == UIntPtr.Zero || theCode.ToUInt64() < 0x10000)
+                return 0;
+
+            if (ReadProcessMemory(mProc.Handle, theCode, memoryTiny, (UIntPtr)2, IntPtr.Zero))
+                return BitConverter.ToInt32(memoryTiny, 0);
+            else
+                return 0;
+        }
+
+        public int ReadByte(string code, string file = "")
+        {
+            byte[] memoryTiny = new byte[1];
+
+            UIntPtr theCode = GetCode(code, file);
+            if (theCode == null || theCode == UIntPtr.Zero || theCode.ToUInt64() < 0x10000)
+                return 0;
+
+            if (ReadProcessMemory(mProc.Handle, theCode, memoryTiny, (UIntPtr)1, IntPtr.Zero))
+                return memoryTiny[0];
+
+            return 0;
+        }
+
+        public bool[] ReadBits(string code, string file = "")
+        {
+            byte[] buf = new byte[1];
+
+            UIntPtr theCode = GetCode(code, file);
+
+            bool[] ret = new bool[8];
+
+            if (theCode == null || theCode == UIntPtr.Zero || theCode.ToUInt64() < 0x10000)
+                return ret;
+
+            if (!ReadProcessMemory(mProc.Handle, theCode, buf, (UIntPtr)1, IntPtr.Zero))
+                return ret;
+
+
+            if (!BitConverter.IsLittleEndian)
+                throw new Exception("Should be little endian");
+
+            for (var i = 0; i < 8; i++)
+                ret[i] = Convert.ToBoolean(buf[0] & (1 << i));
+
+            return ret;
+
+        }
+
+        public int ReadPByte(UIntPtr address, string code, string file = "")
+        {
+            byte[] memory = new byte[4];
+            if (ReadProcessMemory(mProc.Handle, address + LoadIntCode(code, file), memory, (UIntPtr)1, IntPtr.Zero))
+                return BitConverter.ToInt32(memory, 0);
+            else
+                return 0;
+        }
+
+        public float ReadPFloat(UIntPtr address, string code, string file = "")
+        {
+            byte[] memory = new byte[4];
+            if (ReadProcessMemory(mProc.Handle, address + LoadIntCode(code, file), memory, (UIntPtr)4, IntPtr.Zero))
+            {
+                float spawn = BitConverter.ToSingle(memory, 0);
+                return (float)Math.Round(spawn, 2);
+            }
+            else
+                return 0;
+        }
+
+        public int ReadPInt(UIntPtr address, string code, string file = "")
+        {
+            byte[] memory = new byte[4];
+            if (ReadProcessMemory(mProc.Handle, address + LoadIntCode(code, file), memory, (UIntPtr)4, IntPtr.Zero))
+                return BitConverter.ToInt32(memory, 0);
+            else
+                return 0;
+        }
+
+        public string ReadPString(UIntPtr address, string code, string file = "")
+        {
+            byte[] memoryNormal = new byte[32];
+            if (ReadProcessMemory(mProc.Handle, address + LoadIntCode(code, file), memoryNormal, (UIntPtr)32, IntPtr.Zero))
+                return CutString(System.Text.Encoding.ASCII.GetString(memoryNormal));
+            else
+                return "";
+        }
+
+        public T ReadMemory<T>(string address, string file = "")
+        {
+            object ReadOutput = null;
+
+            switch (Type.GetTypeCode(typeof(T)))
+            {
+                case TypeCode.String:
+                    ReadOutput = ReadString(address, file);
+                    break;
+                case TypeCode.Int32:
+                    ReadOutput = ReadInt(address, file);
+                    break;
+                case TypeCode.Int64:
+                    ReadOutput = ReadLong(address, file);
+                    break;
+                case TypeCode.Byte:
+                    ReadOutput = ReadByte(address, file);
+                    break;
+                case TypeCode.Double:
+                    ReadOutput = ReadDouble(address, file);
+                    break;
+                case TypeCode.Decimal:
+                    ReadOutput = ReadFloat(address, file);
+                    break;
+                case TypeCode.UInt32:
+                    ReadOutput = ReadUInt(address, file);
+                    break;
+                default:
+                    break;
+            }
+
+            if (ReadOutput != null)
+                return (T)Convert.ChangeType(ReadOutput, typeof(T));
+            else
+                return default(T);
+        }
+
+        ConcurrentDictionary<string, CancellationTokenSource> ReadTokenSrcs = new ConcurrentDictionary<string, CancellationTokenSource>();
+        public void BindToUI(string address, Action<string> UIObject, string file = "")
+        {
+            CancellationTokenSource cts = new CancellationTokenSource();
+            if (ReadTokenSrcs.ContainsKey(address))
+            {
+                try
+                {
+                    ReadTokenSrcs[address].Cancel();
+                    ReadTokenSrcs.TryRemove(address, out _);
+                }
+                catch
+                {
+                    }
+            }
+            else
+            {
+            }
+
+            ReadTokenSrcs.TryAdd(address, cts);
+
+            Task.Factory.StartNew(() =>
+            {
+                while (!cts.Token.IsCancellationRequested)
+                {
+                    UIObject(ReadMemory<string>(address, file));
+                    Thread.Sleep(100);
+                }
+            },
+            cts.Token);
+        }
+    }
+}
---
+++ MemoryNoAdmin/Structures/Process.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace MemoryNoAdmin
+{
+    public class Proc
+    {
+        public Process Process { get; set; }
+        public IntPtr Handle { get; set; }
+        public bool Is64Bit { get; set; }
+        public ProcessModule MainModule { get; set; }
+    }
+}
